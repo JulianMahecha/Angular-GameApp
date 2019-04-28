5ed1a42b3597889e07df7bb11630d93219cefeed
Julian Dev <jemahechaj@ut.edu.co>
2019-04-28 12:25:19 -0500

    Probando Edit 25823

--- client/src/app/components/game-form/game-form.component.ts
+++ client/src/app/components/game-form/game-form.component.ts
@@ -1,7 +1,7 @@
 import { GamesService } from './../../services/games.service';
 import { Game } from './../../models/Game';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-game-form',
@@ -16,19 +16,29 @@ export class GameFormComponent implements OnInit {
     image: '',
   }
 
-  constructor(private gamesService: GamesService) { }
+  constructor(private gamesService: GamesService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   saveNewGame(){
     this.gamesService.saveGame(this.game)
     .subscribe(
       res=>{
         console.log(res);
+        this.router.navigate(['/games']);
       },
       err => console.error(err)
     );
   }
 
   ngOnInit() {
+    const params = this.activatedRoute.snapshot.params;
+    if(params.id){
+      this.gamesService.getGame(params.id).subscribe(
+        res => {
+          this.game = res;
+        },
+        err => console.error(err)
+      )
+    }
   }
 
 }
